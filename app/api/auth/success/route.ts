a1e63818ecdef70a735801a7f551a933cf014047
omid abbasi <itmanager.abbasi@gmail.com>
2024-05-20 15:15:33 +0330

    Add force-dynamic to route

--- app/api/auth/success/route.ts
+++ app/api/auth/success/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import prisma from "@/app/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
